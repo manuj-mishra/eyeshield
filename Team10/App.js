7d1adc204ae917c8ee82764809abdd4d3c71fb0d
540KJ <jljqiuc@gmail.com>
2020-02-08 14:09:39 +0000

    Added request camera permissions function

--- Team10/App.js
+++ Team10/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {PermissionsAndroid} from 'react-native';
 
 export default function App() {
+  requestCameraPermission();
+  
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -17,3 +20,27 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+async function requestCameraPermission() {
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.CAMERA,
+      {
+        title: 'Cool Photo App Camera Permission',
+        message:
+          'Cool Photo App needs access to your camera ' +
+          'so you can take awesome pictures.',
+        buttonNeutral: 'Ask Me Later',
+        buttonNegative: 'Cancel',
+        buttonPositive: 'OK',
+      },
+    );
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      console.log('You can use the camera');
+    } else {
+      console.log('Camera permission denied');
+    }
+  } catch (err) {
+    console.warn(err);
+  }
+}
